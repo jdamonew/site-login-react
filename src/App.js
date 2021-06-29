067ac0265a010e1c17f801b2e057bd3650e8b7f2
jdamonew <damone556@gmail.com>
2021-06-29 12:40:21 -0300

    Criação da Pagina de Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import imagem from './assets/img-background.png';
+import logo from './assets/logo.png';
+import logoRoxo from './assets/logo-roxo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/*BLOCO ESQUERDO*/}
+      <div className="containerOne">
+        <img src={logo} className='logo'/>
+        <h1>welcome to my site.</h1>
+        <img src={imagem}/>
+      </div>
+
+      {/*BLOCO DIREITO*/}
+      <div className="containerTwo">
+        <img src={logoRoxo} className='logo'/>
+
+        <h1 className="login-title">log in</h1>
+        <p className="login-subtitle">enter with your credentials ou sign up in our site.</p>
+
+        <div className='containerLogin'>
+
+          <label>e-mail</label>
+          <input type='email'></input>
+
+          <label>password</label>
+          <input type='password'></input>
+
+          
+
+        </div>
+     
+     
+      </div>
+
+
     </div>
   );
 }
